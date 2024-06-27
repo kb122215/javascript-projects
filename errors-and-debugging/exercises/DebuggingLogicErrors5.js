acec3a6744689ce84245f89d2316e6f958ed3833
Kareemah Bahaadur <kareemahbahaadur78@gmail.com>
2024-06-27 12:35:49 -0400

    Fixed all the errors

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -1,28 +1,39 @@
 // The value of launchReady assigned in the first if/else block gets changed in the second if/else block. Dangerous waters...
-// Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result. 
+// Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result.
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
 let launchReady = false;
+crewReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
-let computerStatus = 'green';
+let computerStatus = "green";
 
 if (fuelLevel >= 20000) {
-   console.log('Fuel level cleared.');
-   launchReady = true;
+  console.log("Fuel level cleared.");
+  launchReady = true;
 } else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+  console.log("WARNING: Insufficient fuel!");
+  launchReady = false;
 }
 
 console.log("launchReady = ", launchReady);
 
-if (crewStatus && computerStatus === 'green'){
-   console.log('Crew & computer cleared.');
-   launchReady = true;
+if (crewStatus && computerStatus === "green") {
+  console.log("Crew & computer cleared.");
+  crewReady = true;
 } else {
-   console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+  console.log("WARNING: Crew or computer not ready!");
+  crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("crewReady = ", crewReady);
+// final  if/else countdown
+if (launchReady && crewReady) {
+  console.log("All systems go. Countdown to liftoff: ");
+  for (let i = 3; i > 0; i--) {
+    console.log(i);
+  }
+  console.log("Liftoff!");
+} else {
+  console.log("Launch scrubbed. ");
+}
